ba3713a46aedf4f3cbc55c1adf76e420fcad22b1
Harsh Dagar <amandagar000@gmail.com>
2024-07-01 04:05:36 +0530

    add config file for pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+if (!fs.existsSync(path.resolve(__dirname, ".env"))) {
+  console.error("No .env file found.");
+  process.exit(1);
+}
+const env = fs.readFileSync(path.resolve(__dirname, ".env"), "utf8");
+
+const envJson = {};
+env.split("\n").forEach((line) => {
+  const [key, value] = line.split("=");
+  envJson[key] = value.replace(/(\r\n|\n|\r)/gm, "");
+});
+
+module.exports = {
+  apps: [
+    {
+      script: "./build/index.js",
+      env: envJson,
+    },
+  ],
+};
