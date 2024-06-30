61b168afaf4d52658298b68e15ad3ac4072705d2
Harsh Dagar <amandagar000@gmail.com>
2024-07-01 04:05:16 +0530

    moved http certificate to nginx

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -4,9 +4,6 @@ declare global {
       NODE_ENV: "development" | "production" | "test";
       HTTP_PORT: string;
       JWT_SECRET: string;
-      CERTIFICATE: string;
-      CERTIFICATE_KEY: string;
-      CERTIFICATE_PASSPHRASE: string;
       SMTP_SERVER: string;
       SMTP_USERNAME: string;
       SMTP_PASSWORD: string;
--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,4 @@
-import fs from "fs";
 import http from "http";
-import https from "https";
 import express from "express";
 import cors from "cors";
 import { default as authMiddleware } from "./middleware/auth.js";
@@ -9,72 +7,30 @@ import sendEmail, { verifySMTP } from "./lib/mailer.js";
 import { consumeMessage } from "./lib/kafka/consume.js";
 import router from "./routes/index.js";
 const app = express();
-const PORT = process.env.HTTP_PORT || 80;
+const PORT = process.env.HTTP_PORT || 6543;
 
 app.use(cors({ origin: "*" }));
 app.use(authMiddleware);
 app.use(express.json());
 app.use(router);
 
-async function startServer() {
-  const privateKey = await fetch(process.env.CERTIFICATE_KEY).then((res) =>
-    res.text()
-  );
-  const certificate = await fetch(process.env.CERTIFICATE).then((res) =>
-    res.text()
-  );
+const httpServer = http.createServer(app);
 
-  const httpServer = http.createServer(app);
-  const httpsServer = https.createServer(
-    {
-      key: privateKey,
-      passphrase: process.env.CERTIFICATE_PASSPHRASE,
-      cert: certificate,
-    },
-    app
-  );
-
-  httpsServer
-    .listen(443, async () => {
-      console.log(`HTTPS SERVER STARTED AT :443`);
-      verifySMTP(async (err, success) => {
-        if (!err) {
-          await connectProducer();
-          console.log("Producer is connected");
-          await consumeMessage(sendEmail);
-          console.log("Consumer is connected");
-        } else {
-          console.error("SMTP Server is not connected", err);
-        }
-      });
-    })
-    .on("close", async () => {
-      console.log("Server is closing");
-      await disconnectProducer();
-    });
-
-  httpServer
-    .listen(PORT, async () => {
-      console.log(`HTTP SERVER PORT: ${PORT}`);
-      if (PORT === 80) {
-        verifySMTP(async (err, success) => {
-          if (!err) {
-            await connectProducer();
-            console.log("Producer is connected");
-            await consumeMessage(sendEmail);
-            console.log("Consumer is connected");
-          } else {
-            console.error("SMTP Server is not connected", err);
-          }
-        });
-      }
-    })
-    .on("close", async () => {
-      if (PORT === 80) {
-        console.log("Server is closing");
-        await disconnectProducer();
+httpServer
+  .listen(PORT, async () => {
+    console.log(`HTTP SERVER PORT: ${PORT}`);
+    verifySMTP(async (err, success) => {
+      if (!err) {
+        await connectProducer();
+        console.log("Producer is connected");
+        await consumeMessage(sendEmail);
+        console.log("Consumer is connected");
+      } else {
+        console.error("SMTP Server is not connected", err);
       }
     });
-}
-
-startServer();
+  })
+  .on("close", async () => {
+    console.log("Server is closing");
+    await disconnectProducer();
+  });
