6cc8a83e41fc0d10b0e15948bf4e7b2d47dd2ad4
Harsh Dagar <amandagar000@gmail.com>
2024-08-11 03:04:40 +0530

    enabled minify

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,6 +3,7 @@ import esbuild from "esbuild";
 await esbuild.build({
   entryPoints: ["./src/lambda/**/*.ts"],
   bundle: true,
+  minify: true,
   outdir: "build",
   platform: "node",
   target: "node20",
