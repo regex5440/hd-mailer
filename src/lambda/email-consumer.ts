cfda93bbde561b355501713a0461869439107280
Harsh Dagar <amandagar000@gmail.com>
2024-08-11 00:54:46 +0530

    message consumer from sqs

---
+++ src/lambda/email-consumer.ts
@@ -0,0 +1,23 @@
+import { MailData } from "@types";
+import sendEmail from "src/lib/mailer.js";
+import type { Handler, SQSEvent } from "aws-lambda";
+import decryptEnvVar from "src/lib/aws/env-decrypt";
+
+export const handler: Handler = async (event: SQSEvent) => {
+  process.env.SMTP_PASSWORD.endsWith("=")
+    ? await decryptEnvVar("SMTP_PASSWORD")
+    : null;
+  if (event.Records.length === 0) {
+    return;
+  }
+  console.log("Received event", event);
+  try {
+    for (const record of event.Records) {
+      const messageData: MailData = JSON.parse(record.body);
+      sendEmail(messageData);
+    }
+  } catch (error) {
+    console.error("Error while processing message", error);
+  }
+  return;
+};
